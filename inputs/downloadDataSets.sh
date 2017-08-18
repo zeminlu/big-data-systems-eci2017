2be763d2a25ffe907583a9ef96a4345e25b17130
Luciano Zemin <zeminlu@wolox.com.ar>
2017-08-18 15:10:25 -0300

    Script for downloading the DataSets
    Given the DataSets are bigger than what Github allows, they have to be
    downloaded manually.

---
+++ inputs/downloadDataSets.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://data.buenosaires.gob.ar/api/files/flujo-vehicular-2015.csv/download/csv inputs
+wget https://data.buenosaires.gob.ar/api/files/molinetes-2017.csv/download/csv
+wget http://datos.gob.ar/dataset/b8418d41-8e0c-4e85-8aa8-80d51a840132/resource/5e585bd4-bc7c-4ac6-b9a1-bcf44b85b28e/download/historico-nombres.zip
